90f0d439170defc41cebca8e8e786660c061e976
Eric Shantz <eric.shantz@solace.com>
2025-12-12 10:48:14 -0500

    fix font style

--- server.js
+++ server.js
@@ -231,8 +231,6 @@ app.get("/user-stats", async (req, res) => {
     /* -------------------------
        Layout & Utility Classes
     --------------------------*/
-    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@500;600;700&display=swap');
-
     body {
       margin: 0;
       font-family: "Open Sans", sans-serif;
